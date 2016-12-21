67779cc1c5d54402c50d0cbd4f72f0b104c60906
Geek-zy <zy251021@163.com>
2016-12-21 17:06:46 +0800

    省市县插件

---
+++ PCASClass/Linkage.js
@@ -0,0 +1,16 @@
+
+//省市地区联动
+/*
+new PCAS("Province","City");
+new PCAS("Province","City","吉林省");
+new PCAS("Province","City","吉林省","吉林市");
+*/
+//省、市、地区对象取得的值均为实际值。
+//注：省、市、地区提示信息选项的值为""(空字符串)
+new PCAS("Province","City","Area");
+/*
+new PCAS("Province","City","Area","吉林省");
+new PCAS("Province","City","Area","吉林省","松原市");
+new PCAS("Province","City","Area","吉林省","松原市","宁江区");
+*/
+
